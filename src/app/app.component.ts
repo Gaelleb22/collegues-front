f341b5804fee6ff4e2958f025b05c684b3f59a2d
Gaëlle BROSSAS <gaellebrossas@orange.fr>
2020-07-29 15:22:26 +0200

    ajout carousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
 
-@Component({selector: 'ngbd-carousel-basic', templateUrl: './app.component.html'})
+@Component({
+  selector: 'app-ngbd-carousel-basic',
+  templateUrl: './app.component.html'
+})
+
 export class NgbdCarouselBasic {
   images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  tokyo = '/assets/tokyo.JPG';
+  citron = '/assets/citron.JPG';
 }
 
 @Component({
